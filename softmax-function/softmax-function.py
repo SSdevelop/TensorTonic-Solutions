a8ed0d10fc799718a885a68883351774d747f730
SRIVASTAVA Srijan <srijan.srivastav2@gmail.com>
2026-02-01 03:12:47 +0800

    Update solution for softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -10,8 +10,12 @@ def softmax(x):
     # x = np.asarray(x, dtype=float)
     # y = np.atleast_2d(x)
     axis = 1 if x.ndim == 2 else 0
+    # max_x = np.expand_dims(np.max(x, axis=axis), axis)
+    # x = x - max_x
+    # e_x = np.exp(x)
+    # result = e_x / np.expand_dims(np.sum(e_x, axis=axis), axis)
+    # return result
     max_x = np.expand_dims(np.max(x, axis=axis), axis)
     x = x - max_x
     e_x = np.exp(x)
-    result = e_x / np.expand_dims(np.sum(e_x, axis=axis), axis)
-    return result
+    return e_x / np.sum(e_x, axis=axis, keepdims=True)
